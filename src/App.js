6433864075e0c87417bb266250191f22efe6ff7a
Bruce Gordon <bruce@focusls.com>
2021-08-31 17:21:26 -0400

    1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import Navbar from './Navbar';
 import FsFeature from './FSFeature';
 
 function App() {
+  const handleLoginClick = () => {
+    console.log()
+  }
   return (
     <div className="App">
       <Navbar />
+      <input type="text" placeholder="enter user id" id="txtUserId" />
+      <button>Login</button>
       <div className="content">
         <FsFeature />
       </div>
